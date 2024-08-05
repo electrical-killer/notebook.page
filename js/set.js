68e430c5f7c22c3729d5fc600825879278b3246f
hg_it_nuc <2675263825@qq.com>
2024-08-05 12:45:52 +0800

    修复壁纸bug，感谢ET大佬

--- js/set.js
+++ js/set.js
@@ -27,54 +27,87 @@ function getBgImg() {
   }
 }
 
+// 随机选择一张默认背景图片
+function getRandomLocalImg() {
+  let imgIndex = 1 + ~~(Math.random() * 10);
+  return `./img/background${imgIndex}.png`;
+}
+
+// 设置随机本地背景图片
+function setBgImgDefault() {
+  let bg_img = getBgImg();
+  let imgUrl = getRandomLocalImg();
+  let attempts = 0;
+  const maxAttempts = 3;
+
+  // 避免重复随机到同一张图片
+  while (attempts++ < maxAttempts) {
+    if (bg_img[1] === imgUrl) {
+      imgUrl = getRandomLocalImg();
+    } else {
+      bg_img[1] = imgUrl;
+      setBgImg(bg_img);
+      break;
+    }
+  }
+
+  return (callback) => {
+    const img = new Image();
+
+    img.onload = () => {
+      if (img.naturalWidth !== 0) {
+        callback(imgUrl); // 图片加载成功，调用回调函数并传入图片URL
+      } else {
+        callback("./img/background1.png"); // 使用默认图片URL
+      }
+    };
+
+    img.onerror = () => {
+      callback("./img/background1.png"); // 图片加载失败，使用默认图片URL
+    };
+
+    img.src = imgUrl;
+  }
+}
+
 let bg_img_preinstall = {
   type: "1", // 1:默认背景 2:每日一图 3:随机风景 4:随机动漫
+  1: "./img/background2.png", // 默认背景
   2: "https://api.dujin.org/bing/1920.php", // 每日一图
   3: "https://api.btstu.cn/sjbz/api.php?lx=fengjing&format=images", // 随机风景
   4: "https://www.dmoe.cc/random.php", // 随机动漫
 };
 
-// 更改背景图片
+// 初始化背景图片
 function setBgImgInit() {
   let bg_img = getBgImg();
   $("input[name='wallpaper-type'][value=" + bg_img["type"] + "]").click();
 
-  switch (bg_img["type"]) {
-    case "1":
-      $("body").css(
-        "background-image",
-        `url("./img/background${1 + ~~(Math.random() * 10)}.webp")`,
-      );
-      // $("#bg").attr(
-      //   "src",
-      //   `./img/background${1 + ~~(Math.random() * 10)}.webp`
-      // ); //随机默认壁纸
-      break;
-    case "2":
-      $("body").css("background-image", `url("${bg_img_preinstall[2]}")`);
-      // $("#bg").attr("src", bg_img_preinstall[2]); //必应每日
-      break;
-    case "3":
-      $("body").css("background-image", `url("${bg_img_preinstall[3]}")`);
-      // $("#bg").attr("src", bg_img_preinstall[3]); //随机风景
-      break;
-    case "4":
-      $("body").css("background-image", `url("${bg_img_preinstall[4]}")`);
-      // $("#bg").attr("src", bg_img_preinstall[4]); //随机动漫
-      break;
+  if (bg_img["type"] === "1") {
+    setBgImgDefault()((imgUrl) => {
+      console.log(imgUrl);
+      bg_img_preinstall[1] = imgUrl;
+      $("body").css("background-image", `url("${imgUrl}")`);
+    });
+  } else {
+    $("body").css("background-image", `url("${bg_img_preinstall[bg_img["type"]]}")`);
   }
 }
 
-// 立即更改背景图片
+// 即时切换背景图片
 function changeBg(type) {
-  $("body").css("background-image", `url("${bg_img_preinstall[type]}")`);
-}
+  let bg_img = getBgImg();
+  bg_img["type"] = type;
+  setBgImg(bg_img);
 
-// 监听壁纸类型选择
-$("input[name='wallpaper-type']").click(function() {
-  var type=$(this).val();
-  changeBg(type);
-})
+  if (type === "1") {
+    setBgImgDefault()((imgUrl) => {
+      $("body").css("background-image", `url("${imgUrl}")`);
+    });
+  } else {
+    $("body").css("background-image", `url("${bg_img_preinstall[type]}")`);
+  }
+}
 
 $(document).ready(function () {
   // 壁纸数据加载
@@ -82,13 +115,11 @@ $(document).ready(function () {
   // 设置背景图片
   $("#wallpaper").on("click", ".set-wallpaper", function () {
     let type = $(this).val();
-    let bg_img = getBgImg();
-    bg_img["type"] = type;
+    changeBg(type);
     iziToast.show({
       icon: "fa-solid fa-image",
       timeout: 2500,
       message: "壁纸设置成功，刷新后生效",
     });
-    setBgImg(bg_img);
   });
 });
